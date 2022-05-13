fa9775089582a414875830a79dc83e800b845007
Joe Hawkins <hawkinsjwh@gmail.com>
2022-05-13 17:43:23 -0600

    Added another file and fixed the compiler errors.

---
+++ goodMorning.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    // Var is used to declare mutable variables and their data types.
+
+    var todaysDate: String = "05/13/2022"
+    println(todaysDate)
+
+
+    var currentWeather: String
+    currentWeather = "Windy."
+    println(currentWeather)
+
+
+    // Val is used to declare variables that are immutable.
+
+    val pi: Double = 3.14
+    println(pi)
+
+
+    // With Kotlin, Type inference is a common practice, where the compiler takes declared variables that are aren't
+    // explicitly declared, such as var name = "Joe" - will be assumed to be a string. Another example would be
+    // var boolean = true. The compiler assumes that this is a boolean value and doesn't question further - however, errors
+    // will occur if the variable is tried to be changed.
+
+}
