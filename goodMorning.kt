cb7aaed4124cc3d6a2cee21c174e15f6e7502f42
Joe Hawkins <hawkinsjwh@gmail.com>
2022-05-13 18:31:24 -0600

    Tried creating a class...

--- goodMorning.kt
+++ goodMorning.kt
@@ -18,8 +18,30 @@ fun main() {
 
 
     // With Kotlin, Type inference is a common practice, where the compiler takes declared variables that are aren't
-    // explicitly declared, such as var name = "Joe" - will be assumed to be a string. Another example would be
-    // var boolean = true. The compiler assumes that this is a boolean value and doesn't question further - however, errors
+    // explicitly declared, such as:
+    var name = "Joe"
+    // will be assumed to be a string.
+
+    // Another example would be
+    var boolean = true
+    // The compiler assumes that this is a boolean value and doesn't question further - however, errors
     // will occur if the variable is tried to be changed.
+
+
+    // Let's attempt to make a class...
+
+    class Question(
+
+        var points: Int,
+        val question: String,
+        val difficulty: String
+
+
+    )
+
+    // Declaring an object...
+
+
+
 
 }
